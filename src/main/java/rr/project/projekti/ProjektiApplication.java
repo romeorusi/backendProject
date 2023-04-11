e65f0811c320e720a6ec817724efa1d6828ca591
romeorusi <Romeo.rusi@gmail.com>
2023-04-11 17:59:59 +0300

    first commit

---
+++ src/main/java/rr/project/projekti/ProjektiApplication.java
@@ -0,0 +1,13 @@
+package rr.project.projekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rr/project/projekti/ProjektiApplicationTests.java
@@ -0,0 +1,13 @@
+package rr.project.projekti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
